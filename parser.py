bf394f2f12ba1f13f6c6332bb5ea1f342198adfa
michaellikaiye <mye1@stuy.edu>
2019-04-04 15:01:02 -0400

    hu

--- parser.py
+++ parser.py
@@ -167,12 +167,18 @@ def parse_file( fname, edges, polygons, csystems, screen, color ):
         elif line == 'scale':
             #print 'SCALE\t' + str(args)
             t = make_scale(float(args[0]), float(args[1]), float(args[2]))
-            matrix_mult(t, cs)
+            top = s.pop()
+            matrix_mult(top, t)
+            s.push(t)
+            cs = s.peek()
 
         elif line == 'move':
             #print 'MOVE\t' + str(args)
             t = make_translate(float(args[0]), float(args[1]), float(args[2]))
-            matrix_mult(t, cs)
+            top = s.pop()
+            matrix_mult(top, t)
+            s.push(t)
+            cs = s.peek()
 
         elif line == 'rotate':
             #print 'ROTATE\t' + str(args)
@@ -184,7 +190,10 @@ def parse_file( fname, edges, polygons, csystems, screen, color ):
                 t = make_rotY(theta)
             else:
                 t = make_rotZ(theta)
-            matrix_mult(t, cs)
+            top = s.pop()
+            matrix_mult(top, t)
+            s.push(t)
+            cs = s.peek()
 
         elif line == 'display':
             display(screen)
